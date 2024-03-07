d85c8235b1dd2a3c70b470b952c9a1699f5bc724
XxGabrielerochaxX <100355437+XxGabrielerochaxX@users.noreply.github.com>
2024-03-06 21:57:19 -0300

    ex
    primeiro exercicio

---
+++ src/main/java/com/example/estruturadedadosav/EstruturaDeDadosAvApplication.java
@@ -0,0 +1,13 @@
+package com.example.estruturadedadosav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstruturaDeDadosAvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstruturaDeDadosAvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/estruturadedadosav/EstruturaDeDadosAvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estruturadedadosav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstruturaDeDadosAvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
